e3c0c997049dd6291cfd5e11e8fbeb2893cbcd94
foysaliuice <foysal.iuice@gmail.com>
2017-09-29 21:11:16 +0600

    Add redult method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class StudentController extends Controller
 {
     public function index(){
       return view('student');
+    }
+
+    public function result(){
+      $student_data = DB::table('tbl_student_info')
+                        ->select('*')
+                        ->where('std_name', 'like', '%'.$_POST["search"].'%')
+                        ->get();
+
+      return $student_data;
     }
 }
